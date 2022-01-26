f9ea5056803fbdb77ab7fe7bc995d799af8d429b
usercao <cao858646882@qq.com>
2022-01-26 16:25:37 +0800

    update

--- src/models/_util.ts
+++ src/models/_util.ts
@@ -1,46 +1,18 @@
-import { AtomEffect, DefaultValue } from 'recoil'
+import { AtomEffect, DefaultValue } from 'recoil';
 
 export const localStorageEffect: <T>(key: string) => AtomEffect<T> =
   (key: string) =>
   ({ setSelf, onSet }) => {
-    const savedValue = window.localStorage.getItem(key)
+    const savedValue = window.localStorage.getItem(key);
     if (savedValue != null) {
-      setSelf(JSON.parse(savedValue))
+      setSelf(JSON.parse(savedValue));
     }
 
     onSet((newValue) => {
       if (newValue instanceof DefaultValue) {
-        window.localStorage.removeItem(key)
+        window.localStorage.removeItem(key);
       } else {
-        window.localStorage.setItem(key, JSON.stringify(newValue))
+        window.localStorage.setItem(key, JSON.stringify(newValue));
       }
-    })
-  }
-
-export const localForageEffect: <T>(key: string) => AtomEffect<T> =
-  (key: string) =>
-  ({ setSelf, onSet }) => {
-    const loadPersisted = async () => {
-      const savedValue = await window.localStorage.getItem(key)
-
-      if (savedValue != null) {
-        setSelf(JSON.parse(savedValue))
-      }
-    }
-
-    loadPersisted()
-
-    onSet((newValue) => {
-      if (newValue instanceof DefaultValue) {
-        window.localStorage.removeItem(key)
-      } else {
-        window.localStorage.setItem(key, JSON.stringify(newValue))
-      }
-    })
-  }
-
-// export const authenticationInfoState = atom<AuthenticationInfo | null>({
-//   key: 'authenticationInfoState',
-//   default: null,
-//   effects_UNSTABLE: [localStorageEffect<AuthenticationInfo | null>(LOCAL_STORAGE_KEY)],
-// })
+    });
+  };
