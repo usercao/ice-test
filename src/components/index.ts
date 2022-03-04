24404137a7b7128a01485d2d92e54590a1cf3edc
usercao <cao858646882@qq.com>
2022-03-04 18:58:44 +0800

    update

--- src/components/index.ts
+++ src/components/index.ts
@@ -7,16 +7,19 @@ export type { InputProps } from './Input';
 export { default as Select } from './Select';
 export type { SelectProps } from './Select';
 
-export { default as Radio } from './Radio';
-export type { RadioProps } from './Radio';
+export { default as Radio } from './Checkbox';
+export type { RadioProps } from './Checkbox';
 
-export { default as Tooltip } from './Tooltip';
-export type { TooltipProps } from './Tooltip';
+export { default as Scrollbar } from './Scrollbar';
+export type { ScrollbarProps } from './Scrollbar';
 
 export { default as Modal } from './Modal';
 export type { ModalProps } from './Modal';
 
-export { default as Scrollbar } from './Scrollbar';
-export type { ScrollbarProps } from './Scrollbar';
+// export { default as Radio } from './Radio';
+// export type { RadioProps } from './Radio';
+
+// export { default as Tooltip } from './Tooltip';
+// export type { TooltipProps } from './Tooltip';
 
 // export { default as message } from './Message'
