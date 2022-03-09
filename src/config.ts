adf3bd24f242ca2fa537c83a0b3d9f9659a4ccd5
guoyuhao <969525633@qq.com>
2022-03-09 10:39:43 +0800

    config

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,10 @@
 export default {
   default: {
-    api: '/api',
-    apiV1: '/api/v1',
+    VITE_API: '/api',
+    VITE_API_V1: '/api/v1',
   },
   prod: {
-    api: '/api',
-    apiV1: '/api/v1',
+    VITE_API: '/api',
+    VITE_API_V1: '/api/v1',
   },
 };
--- src/config/api.ts
+++ src/config/api.ts
@@ -1,15 +1,17 @@
 import { config } from 'ice';
 
+const { VITE_API, VITE_API_V1 } = config;
+
 export default {
   // send code
-  SendEmailNotLogin: `${config.api}/common/send_email_verify_code`,
+  SendEmailNotLogin: `${VITE_API}/common/send_email_verify_code`,
 
   // login
-  usernameLogin: `${config.api}/user/authorize`,
-  loginVerify: `${config.api}/user/authorize_advance`,
-  loginQrCode: `${config.api}/user/get_loginQrCode`,
-  qrCodeResult: `${config.api}/user/get_scan_login_qrcode_result`,
+  usernameLogin: `${VITE_API}/user/authorize`,
+  loginVerify: `${VITE_API}/user/authorize_advance`,
+  loginQrCode: `${VITE_API}/user/get_loginQrCode`,
+  qrCodeResult: `${VITE_API}/user/get_scan_login_qrcode_result`,
 
   // sign up
-  signUp: `${config.api}/user/email_signUp`,
+  signUp: `${VITE_API}/user/email_signUp`,
 };
