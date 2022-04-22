4471014dd804ad2431541fd7eab3b1e93a877ab6
usercao <cao858646882@qq.com>
2022-04-22 14:45:49 +0800

    update

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,12 @@
 import * as React from 'react';
 import styled from 'styled-components';
-import { Modal } from '@/components';
+import { Modal, message } from '@/components';
 import { useHistory } from 'ice';
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+`;
 
 const Home = () => {
   const history = useHistory();
@@ -12,7 +15,13 @@ const Home = () => {
 
   return (
     <Wrapper className="col-center">
-      <button onClick={() => history.push('/login')}>登录</button>
+      <button
+        onClick={() => {
+          message.success('');
+        }}
+      >
+        message
+      </button>
       {/* <Scrollbar
         trackStyle={(horizontal) => ({ [horizontal ? 'height' : 'width']: 0 })}
         thumbStyle={(horizontal) => ({ [horizontal ? 'height' : 'width']: 4 })}
@@ -24,7 +33,7 @@ const Home = () => {
         <div>212121212121</div>
       </Scrollbar> */}
       {/* <i className="iconfont icon-phone"></i> */}
-      <button onClick={() => setModalVisible(true)}>弹窗</button>
+      <button onClick={() => setModalVisible(true)}>modal</button>
       <Modal visible={modalVisible} title="212121" cancel="1111" ok="222">
         <div onClick={() => setModalVisible(false)} style={{ width: 400, height: 400 }}>
           close
